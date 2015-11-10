7785bc5807febe9777c33da38b419b49162bc72f
Victor Hernandez Perez <alu0100697032.edu.es>
2015-11-10 18:28:29 +0000

    Finished

--- animacion.js
+++ animacion.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
     $("#mic").click(function(){
         $(this).css("background-color", "#2E9AFE");
-        //$(this).animate({bottom: '200px'});
     });
 });
+
+function animar(interimResult, recognition){
+    if(interimResult.indexOf('derecha') != -1)
+        $("#mic").animate({left: '+=10%', right: '-=10p%'});
+    if(interimResult.indexOf('izquierda') != -1)
+        $("#mic").animate({right: '+=10%', left: '-=10%'});
+    if(interimResult.indexOf('arriba') != -1)
+        $("#mic").animate({bottom: '+=10%', top: '-=10%'});
+    if(interimResult.indexOf('abajo') != -1)
+        $("#mic").animate({top: '+=10%', bottom: '-=10%'});
+    if(interimResult.indexOf('ocultar') != -1)
+        $("#mic").hide();
+    if(interimResult.indexOf('mostrar') != -1)
+        $("#mic").show();
+    if (interimResult.indexOf('terminar') != -1){
+        $("#mic").css("background-color", "#FF0000");
+        $("#mic").animate({
+            top:'49%',
+            bottom:'49%',
+            left:'45%',
+            right:'45%',});
+            recognition.stop();
+        }
+}
---
+++ recognition.js
@@ -0,0 +1,80 @@
+window.onload = function () {
+
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = "es";
+
+    mic.addEventListener('click', function () {
+        areaResult.focus();
+        recognition.start();
+    });
+
+    //events
+
+    recognition.onaudiostart = function (event) {
+        console.log("onaudiostart");
+    };
+
+    recognition.onsoundstart = function (event) {
+        console.log("onsoundstart");
+    };
+
+    recognition.onspeechstart = function (event) {
+        console.log("onspeechstart");
+    };
+
+    recognition.onspeechend = function (event) {
+        console.log("onspeechend");
+    };
+
+    recognition.onsoundend = function (event) {
+        console.log("onsoundend");
+
+    };
+
+    recognition.onaudioend = function (event) {
+        console.log("onaudioend");
+
+    };
+
+    recognition.onresult = function (event) {
+        console.log("onresult");
+        var interimResult = '',
+            finalResult = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                finalResult = event.results[i][0].transcript;
+            } else {
+                interimResult += event.results[i][0].transcript;
+            }
+        }
+
+        finalTranscript.value = finalResult;
+        areaResult.value = interimResult;
+        
+        //Estas dos lineas deberían permitir mover el boton del micro
+        //console.log(interimResult);
+        console.log(interimResult.lastIndexOf('derecha'));
+        
+        animar(interimResult, recognition);
+    };
+
+    recognition.onnomatch = function (event) {
+        console.log("onnomatch");
+    };
+
+    recognition.onerror = function (event) {
+        console.log("onerror: " + event);
+
+    };
+
+    recognition.onstart = function (event) {
+        console.log("onstart");
+    };
+
+    recognition.onend = function (event) {
+        console.log("onend");
+    };
+    
+};
